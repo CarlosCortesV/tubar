0af5c02f4ce65a459ac8ee658600836162aa51d3
CarlosCortesV <carloscortesvera11@gmail.com>
2024-11-24 23:06:06 -0500

    imagenes bares

--- routes/barRoutes.js
+++ routes/barRoutes.js
@@ -7,7 +7,7 @@ const dbConfig = require('../config/dbConfig');
 router.get('/', async (req, res) => {
     try {
         let pool = await sql.connect(dbConfig);
-        let result = await pool.request().query('SELECT id_bar, nombre_bar, ubicación, teléfono, email_contacto, descripción, horario_apertura, horario_cierre, capacidad_maxima FROM bar');
+        let result = await pool.request().query('SELECT id_bar, nombre_bar, ubicación, teléfono, email_contacto, descripción, horario_apertura, horario_cierre, capacidad_maxima,imagen_url FROM Bar');
         res.json(result.recordset); // Envía los datos de los bares en formato JSON
     } catch (err) {
         res.status(500).send('Error al obtener los bares');
